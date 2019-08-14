7733f9177003aa80e147907c7beb28adabc55d11
Drake Rentrop <drakerentrop@gmail.com>
2019-08-14 15:34:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,41 @@
 # require libraries/modules here
 
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class Project
+  @@all = []
+  attr_accessor :image_link, :descrption, :location, :percent_funded
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all?
+    @@all?
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
+
 def create_project_hash
   # write your code here
-end
+
+
+  doc = Nokogiri::HTML(open("fixtures/kickstarter.html"))
+
+  projects = {}
+
+  doc.css("post").each do |post|
+    project = Project.new
+    project.image_link = post.css("a").text
+    project.descrption = post.css("bbcard_blurb").text
+    project.location = post.css("location-name").text
+    project.percent_funded = post.css("strong").text
+    projects << project
+  end
+  projects
+  end
