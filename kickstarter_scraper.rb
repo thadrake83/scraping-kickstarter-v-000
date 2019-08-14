49bc502cf3c32a74130a5b659b8499a5b24d22b4
Drake Rentrop <drakerentrop@gmail.com>
2019-08-14 15:41:13 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,38 +4,48 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-class Project
-  @@all = []
-  attr_accessor :image_link, :descrption, :location, :percent_funded
-
-  def initialize
-    @@all << self
-  end
-
-  def self.all?
-    @@all?
-  end
-
-  def self.reset_all
-    @@all.clear
-  end
-end
+# class Project
+#   @@all = []
+#   attr_accessor :image_link, :descrption, :location, :percent_funded
+# 
+#   def initialize
+#     @@all << self
+#   end
+# 
+#   def self.all
+#     @@all
+#   end
+# 
+#   def self.reset_all
+#     @@all.clear
+#   end
+# 
+#  end
 
 def create_project_hash
   # write your code here
 
-
-  doc = Nokogiri::HTML(open("fixtures/kickstarter.html"))
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  
+  #doc = Nokogiri::HTML(open("fixtures/kickstarter.html"))
 
   projects = {}
 
-  doc.css("post").each do |post|
-    project = Project.new
-    project.image_link = post.css("a").text
-    project.descrption = post.css("bbcard_blurb").text
-    project.location = post.css("location-name").text
-    project.percent_funded = post.css("strong").text
-    projects << project
+  kickstarter.css(li.project.grid_4).each do |project|
+    projects[project] = {}
   end
-  projects
-  end
+  projects 
+end
+  #
+  # doc.css("post").each do |post|
+  #   project = Project.new
+  #   project.image_link = post.css("a").text
+  #   project.descrption = post.css("bbcard_blurb").text
+  #   project.location = post.css("location-name").text
+  #   project.percent_funded = post.css("strong").text
+  #   projects << project
+  # end
+  #
+  # projects
+
