4bbdcf7f679b1f26a0d2aed4eeae32c6eda71390
Drake Rentrop <drakerentrop@gmail.com>
2019-08-14 15:41:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -32,8 +32,9 @@ def create_project_hash
 
   projects = {}
 
-  kickstarter.css(li.project.grid_4).each do |project|
-    projects[project] = {}
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
   end
   projects
 end
